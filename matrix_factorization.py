cc97e1439dede9442f830dcee7183eeee26480a2
KingDragonMoon <wanglongyue@gmail.com>
2025-08-08 21:22:25 -0400

    Update matrix_factorization.py
    matrix fac stuff, no writeup yet

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import sqrtm
+from matplotlib import pyplot as plt
 
 from utils import (
     load_train_csv,
@@ -70,7 +71,7 @@ def update_u_z(train_data, lr, u, z):
     :param train_data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
     :param lr: float
-    :param u: 2D matrix
+    :param u: 2D matrix 
     :param z: 2D matrix
     :return: (u, z)
     """
@@ -84,13 +85,17 @@ def update_u_z(train_data, lr, u, z):
     c = train_data["is_correct"][i]
     n = train_data["user_id"][i]
     q = train_data["question_id"][i]
+
+    #gradient = 2("x" - c)
+    u[n] = u[n] - (lr * (u[n] - c))
+    z[q] = z[q] - (lr * (z[q] - c))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
     return u, z
 
 
-def als(train_data, k, lr, num_iteration):
+def als(train_data, val_data, k, lr, num_iteration):
     """Performs ALS algorithm, here we use the iterative solution - SGD
     rather than the direct solution.
 
@@ -113,11 +118,21 @@ def als(train_data, k, lr, num_iteration):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    mat = None
+    train_loss = []
+    val_loss = []
+
+    for i in range(num_iteration):
+        #Run SGD
+        u, z = update_u_z(train_data, lr, u, z)
+        train_loss.append(squared_error_loss(train_data, u, z))
+        val_loss.append(squared_error_loss(val_data, u, z))
+
+    mat = np.dot(u, np.transpose(z))
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
-    return mat
+    return mat, train_loss, val_loss
 
 
 def main():
@@ -131,7 +146,23 @@ def main():
     # (SVD) Try out at least 5 different k and select the best k        #
     # using the validation set.                                         #
     #####################################################################
-    pass
+
+    #ks = [1, 6, 11, 16, 21, 26, 30]
+    ks = [26]
+    best_k_s = 0
+    best_a_s = 0
+    # Find the most accurate k value
+    for k in ks:
+        svd = svd_reconstruct(train_matrix, k)
+        out = sparse_matrix_evaluate(val_data, svd)
+        if out > best_a_s:
+            best_k_s, best_a_s = k, out
+        print("Validation Accuracy: {}".format(out))
+        test_out = sparse_matrix_evaluate(test_data, svd_reconstruct(train_matrix, best_k_s))
+        print(f"test performance: {test_out}")
+    print(f"Best k: {best_k_s}")
+    print(f"Best accuracy: {best_a_s}")
+    
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -141,7 +172,41 @@ def main():
     # (ALS) Try out at least 5 different k and select the best k        #
     # using the validation set.                                         #
     #####################################################################
-    pass
+    best_k_a = 0
+    best_a_a = 0
+    best_mat = 0
+    best_training_loss = 0
+    best_validation_loss = 0
+    #      vvvv number of repetitions here
+    reps = 3500
+    
+    #Loop through k values and store best
+    for k in ks:
+        mat, training_loss, validation_loss = als(train_data, val_data, k, 0.015, reps)
+        print("Done ALS")
+        als_val = sparse_matrix_evaluate(val_data, mat)
+        if als_val > best_a_a:
+            best_k_a, best_a_a, best_mat, best_training_loss, best_validation_loss = k, als_val, mat, training_loss, validation_loss
+        print("Validation Accuracy: {}".format(als_val))
+    
+    #Plots and terminal output
+    print(f"Best k: {best_k_a}")
+    print(f"learning rate: TBA")
+    print(f"number of iterations: TBA")
+    print(f"Best accuracy: {best_a_a}")
+    als_test = sparse_matrix_evaluate(test_data, best_mat)
+    print(f"test performance: {als_test}")
+    print(f"validation performance: {best_a_a}")
+    
+   
+    #Plot
+    plt.plot(np.arange(reps), best_training_loss, label="Training Loss")
+    plt.plot(np.arange(reps), best_validation_loss, label="Validation Loss")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+    
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
